8e98e8c62bc345bcce108a6a5211d23941d7369a
Zach Sherman <zach@anemon.es>
2020-02-14 18:07:56 -0800

    fix logout

--- src/routes/logout.svelte
+++ src/routes/logout.svelte
@@ -1,11 +1,12 @@
-<script context="module">
-  export async function preload() {
-    await this.fetch('/api/logout')
-  }
-</script>
 <script>
-  import { goto } from '@sapper/app'
+  import { goto, stores } from '@sapper/app'
   import { onMount } from 'svelte'
-  onMount(() => goto('/'))
+  const { session } = stores()
+
+  onMount(async () => {
+    await fetch('/api/logout')
+    $session.token = false
+    goto('/')
+  })
 </script>
 <p>beeep boop good bye my love...</p>
